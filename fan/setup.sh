b87826210b98f614a822ca37e6991b742b7c4681
Aaron Yoon <awesomeyooner@gmail.com>
2026-06-18 22:31:20 -0700

    Update setup.sh

--- fan/setup.sh
+++ fan/setup.sh
@@ -4,9 +4,9 @@ SCRIPT_PATH="$(realpath "$0")"
 SCRIPT_DIR="$(dirname "$SCRIPT_PATH")"
 
 echo "Allowing executables..."
-sudo chmod +x create_service.sh
-sudo chmod +x enable.sh
-sudo chmod +x disable.sh
+sudo chmod +x $SCRIPT_DIR/create_service.sh
+sudo chmod +x $SCRIPT_DIR/enable.sh
+sudo chmod +x $SCRIPT_DIR/disable.sh
 echo "Done!"
 
 # Create and load the actual service file
